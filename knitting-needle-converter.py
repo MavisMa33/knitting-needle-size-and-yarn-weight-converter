e8568313b2f920ec881a758e89c803cc7e79c6f2
Mavis Ma <108603421+MavisMa33@users.noreply.github.com>
2023-03-26 08:39:41 -0700

    Add sort function and update

--- knitting-needle-converter.py
+++ knitting-needle-converter.py
@@ -1,32 +1,46 @@
-jp_to_mm_lab = {0: 2.10, 1: 2.40, 2: 2.70, 3: 3.00, 4: 3.30, 5: 3.60, 6: 3.90, 7: 4.20, 8: 4.50, 9: 4.80, 10: 5.10, 11: 5.40, 12: 5.70, 13: 6.00, 14: 6.30, 15: 6.60}
+jp_mm = {0: 2.10, 1: 2.40, 2: 2.70, 3: 3.00, 4: 3.30, 5: 3.60, 6: 3.90, 7: 4.20, 8: 4.50, 9: 4.80, 10: 5.10, 11: 5.40, 12: 5.70, 13: 6.00, 14: 6.30, 15: 6.60}
 
-us_to_mm_lab = {0: 2.00, 1: 2.25, 2: 2.75, 3: 3.25, 4: 3.50, 5: 3.75, 6: 4.00, 7: 4.50, 8: 5.00, 9: 5.50, 10: 6.00, 10.5: 6.50, 11: 8.00, 13: 9.00, 15: 10.00, 17: 12.75, 19: 15.00, 35: 19.00, 50: 25.00}
+us_mm = {0: 2.00, 1: 2.25, 2: 2.75, 3: 3.25, 4: 3.50, 5: 3.75, 6: 4.00, 7: 4.50, 8: 5.00, 9: 5.50, 10: 6.00, 10.5: 6.50, 11: 8.00, 13: 9.00, 15: 10.00, 17: 12.75, 19: 15.00, 35: 19.00, 50: 25.00}
 
-def jp_to_mm(user_input):
+def sort_jp_mm(jp_mm):
+    sorted_jp_mm = {}
+    for key in sorted(jp_mm.keys()):
+        sorted_jp_mm[key] = jp_mm[key]
+    return sorted_jp_mm
 
-    if user_input in jp_to_mm_lab:
+def sort_us_mm(us_mm):
+    sorted_us_mm = {}
+    for key in sorted(us_mm.keys()):
+        sorted_us_mm[key] = us_mm[key]
+    return sorted_us_mm
+
+def jp_mm_converter(user_input):
+
+    if user_input in jp_mm:
         print("Knitting needle JP size: " + str(user_input))
-        return "Knitting needle size in millimeters: " + str(jp_to_mm_lab[user_input]) + "mm"
+        return "Knitting needle size in millimeters: " + str(jp_mm[user_input]) + "mm"
    
     return "Please check the size number and try it again."
 
-def us_to_mm(user_input):
+def us_mm_converter(user_input):
 
-    if user_input in us_to_mm_lab:
+    if user_input in us_mm:
         print("Knitting needle US size: " + str(user_input))
-        return "Knitting needle size in millimeters: " + str(us_to_mm_lab[user_input]) + "mm"
+        return "Knitting needle size in millimeters: " + str(us_mm[user_input]) + "mm"
    
     return "Please check the size number and try it again."
 
-def us_to_jp(user_input):
+def us_jp_converter(user_input):
 
-    if user_input in us_to_mm_lab:
+    sorted_jp_mm = sort_jp_mm(jp_mm)
+
+    if user_input in us_mm:
 
         smallestAbs = float("inf")
         smallest_jp = []
 
-        for jp_size in jp_to_mm_lab:
-            dif = abs(jp_to_mm_lab[jp_size] - us_to_mm_lab[user_input])
+        for jp_size in sorted_jp_mm:
+            dif = abs(sorted_jp_mm[jp_size] - us_mm[user_input])
             if dif < smallestAbs:
                 smallest_jp = []
                 smallest_jp.append(jp_size)
@@ -51,15 +65,17 @@ def us_to_jp(user_input):
     
     return "Please check the size number and try it again."
 
-def jp_to_us(user_input):
+def jp_us_converter(user_input):
 
-    if user_input in jp_to_mm_lab:
+    sorted_us_mm = sort_us_mm(us_mm)
+
+    if user_input in jp_mm:
 
         smallest_us = []
         smallestAbs = float("inf")
 
-        for us_size in us_to_mm_lab:
-            dif = abs(us_to_mm_lab[us_size] - jp_to_mm_lab[user_input])
+        for us_size in sorted_us_mm:
+            dif = abs(sorted_us_mm[us_size] - jp_mm[user_input])
             if dif < smallestAbs:
                 smallest_us = []
                 smallest_us.append(us_size)
@@ -72,7 +88,4 @@ def jp_to_us(user_input):
         return "The nearest US size is " + str(smallest_us) + "."
     
     return "Please check the size number and try it again."
-
-print(jp_to_us(-6))
-
 
