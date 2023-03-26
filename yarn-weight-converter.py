92eace1a6612e5f33637ef060076c1bae56f5777
Mavis Ma <108603421+MavisMa33@users.noreply.github.com>
2023-03-26 09:46:34 -0700

    Add yarn weight class.

---
+++ yarn-weight-converter.py
@@ -0,0 +1,11 @@
+class Yarn_weight:
+    def __init__(self, name, us, jp, ch, needle_size, crochet_size):
+        self.name = name
+        self.us = us
+        self.jp = jp
+        self.ch = ch
+        self.needle_size = needle_size
+        self.crochet_size = crochet_size
+
+yarn1 = Yarn_weight("Lace", "Light Figering, Cobweb, Laceweight, 1ply, 2ply", "極細", "极细", "1.5~2-25mm", "Lace")
+yarn2 = Yarn_weight("Super Fine", "Sock, Fingering, Baby, 3ply, 4ply", "合細、中細", "细，中细", "2.25~3.25mm", "2.25~3.25mm")
